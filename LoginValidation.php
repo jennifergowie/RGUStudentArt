e98de5459c481cf3047f8bf4b72fe0a043bbf308
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 20:45:59 +0100

    Add files via upload

--- LoginValidation.php
+++ LoginValidation.php
@@ -25,7 +25,7 @@ if(checkPassword($password,$userPassword)) //Check if password is correct and ac
 {
 
     $_SESSION["username"]=$username;
-    $_SESSION["emailaddress"]=$emailaddress;
+    $_SESSION["emailaddress"]=$password;
     header("location: /Home.php");
     exit();
 }
