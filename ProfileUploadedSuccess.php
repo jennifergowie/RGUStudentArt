8848633b56870ec39c499872bb40ac03020668ab
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 14:16:47 +0100

    Add files via upload

--- ProfileUploadedSuccess.php
+++ ProfileUploadedSuccess.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jennygowie123
- * Date: 18/04/2017
- * Time: 13:43
- */
+<!DOCTYPE html>
+
+<!-- NB: add comments, sort formatting! -->
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>New Account Created</title>
+        <link rel="stylesheet" type="text/css" href="Resources/CSS/styleSheet.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rakkas|Quicksand">
+
+    </head>
+
+    <body>
+
+    <main>
+        <div class="bg">
+            <main>
+                <div class="container">
+                    <h4>Your Profile has been created</h4>
+
+                    <h4>Click here to <a href="login.php">login</a></h4>
+
+            <?php
+            include("resources/HeaderandFooterSections/RGUArtistFooterSection.php");
+            ?>
+</div>
+</main>
+</body>
+
+</html>
