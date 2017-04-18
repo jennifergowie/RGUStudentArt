6be3c90af060ebcba81ca55ffc377c629b1f9c38
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 21:22:54 +0100

    Add files via upload

--- Home.php
+++ Home.php
@@ -10,12 +10,15 @@
 
 <body>
 
-<?php
-include("LoggedInHeader.php");
-?>
+
 
 <main>
     <h2>Welcome RGU Student Art</h2>
+
+    <?php
+    include("LoggedInHeader.php");
+    ?>
+    
     <h4>Please click <a href="profiles.php">here </a> to edit/review your personal profile</h4>
     <h4>Please click <a href="ProfilePicture.php">here </a> to upload a profile picture</h4>
     <h4>Please click <a href="slideshow.php"> here </a> to edit/add a slide show</h4>
