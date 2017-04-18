ba675882f7ec9bceecfee7ceae73e210ba9688b0
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 18:43:49 +0100

    Add files via upload

--- ProfilePicture.php
+++ ProfilePicture.php
@@ -1,9 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
-echo "image page";
-/**
- * Created by PhpStorm.
- * User: Jennygowie123
- * Date: 18/04/2017
- * Time: 18:18
- */
+<form action="add.php">
+    <input type="file" name="pic" accept="image/*">
+    <input type="submit">
+</form>
+
+<p><strong>Note:</strong> The accept attribute of the input tag is not supported in Internet Explorer 9 (and earlier versions), and Safari 5 (and earlier).</p>
+<p><strong>Note:</strong> Because of security issues, this example will not allow you to upload files.</p>
+
+</body>
+</html>
