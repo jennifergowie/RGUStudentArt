e6a7507fab31ac540c7905144366d19de4ede770
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 14:00:11 +0100

    Add files via upload

--- UploadProfile.php
+++ UploadProfile.php
@@ -9,8 +9,7 @@ $emailAddress=$_POST["email"]; //Get email address that has been entered
 $password=$_POST["password"]; //Get password that has been entered
 $mobileNumber=$_POST["phone"]; //Get mobile number that has been entered
 
-
-$sql = "INSERT INTO usersprofiles (UserName, EmailAddress, Password, MobileNumber,StudentName,Course,CourseYear)VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."', '".$name."', '".$course."', '".$courseyear."')";
+$sql = "INSERT INTO userprofiles (UserName, EmailAddress, Password, MobileNumber) VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."')";
 $link->query($sql);
 
 echo $username;
@@ -18,4 +17,4 @@ echo $emailAddress;
 echo $password;
 echo $mobileNumber;
 
-header("location: ProfileUploadedSuccess.php");
+header("location: RegisterSuccess.php");
