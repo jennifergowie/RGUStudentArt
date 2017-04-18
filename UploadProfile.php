90566051eaeee128eb161043c3e6c3923f5b69cc
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 16:07:09 +0100

    Add files via upload

--- UploadProfile.php
+++ UploadProfile.php
@@ -11,6 +11,7 @@ $mobileNumber=$_POST["phone"]; //Get mobile number that has been entered
 $StudentName =$_POST["StudentName"];
 $Course = $_POST["Course"];
 $Year = $_POST["Year"];
+$ProfilePicture =$POST["ProfilePicture"];
 
 $sql = "INSERT INTO userprofiles (UserName, EmailAddress, Password, MobileNumber,StudentName,Course,Year) VALUES ('".$username."','".$emailAddress."','".$password."', '".$mobileNumber."', '".$StudentName."', '".$Course."', '".$Year."')";
 $link->query($sql);
--- profiles.php
+++ profiles.php
@@ -13,7 +13,7 @@
         <br/>
         <img style="vertical-align: top; display: inline; height: 75px; width: 300px" src="Resources/Images/Robert_Gordon_University.png" alt="Logo">
         <h2>Create New Account</h2>
-            <form action="UploadProfile.php" method="post" autocomplete="off">
+            <form action="UploadProfile.php" method="post" autocomplete="off" enctype="multipart/form-data">
             <input type="username" name="username" placeholder="Username" onfocus="this.placeholder=''" onblur="this.placeholder='Username'" required/>
             <input type="email" name="email" placeholder="Email Address" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" required/>
             <input type="password" name="password" placeholder="Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" required/>
@@ -21,7 +21,10 @@
             <input type="StudentName" name="StudentName" placeholder="Student Name" onfocus="this.placeholder=''" onblur="this.placeholder='StudentName'" required/>
             <input type="Course" name="Course" placeholder="Course" onfocus="this.placeholder=''" onblur="this.placeholder='Course'" required/>
             <input type="Year" name="Year" placeholder="Year" onfocus="this.placeholder=''" onblur="this.placeholder='Year'" required/>
-            <br><br>
+                <input type="file" name="fileToUpload" id="fileToUpload">
+
+
+                <br><br>
             <input class="inputButton" type="submit" name="submit" value="Create your profile"/>
         </form>
     </div>
