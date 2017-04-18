a51ae63bf999d2cb3fbb6f8c1b69134b773d2faf
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 14:39:58 +0100

    Add files via upload

--- profiles.php
+++ profiles.php
@@ -18,9 +18,9 @@
             <input type="email" name="email" placeholder="Email Address" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" required/>
             <input type="password" name="password" placeholder="Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" required/>
             <input type="tel" name="phone" placeholder="Mobile Number" onfocus="this.placeholder=''" onblur="this.placeholder='Mobile Number'" required/>
-            <input type="StudentName" name="StudentName" placeholder="StudentName" onfocus="this.placeholder=''" onblur="this.placeholder='name'" required/>
-            <input type="course" name="course" placeholder="course" onfocus="this.placeholder=''" onblur="this.placeholder='course'" required/>
-            <input type="year" name="year" placeholder="year" onfocus="this.placeholder=''" onblur="this.placeholder='year'" required/>
+            <input type="StudentName" name="StudentName" placeholder="Student Name" onfocus="this.placeholder=''" onblur="this.placeholder='StudentName'" required/>
+            <input type="Course" name="Course" placeholder="Course" onfocus="this.placeholder=''" onblur="this.placeholder='Course'" required/>
+            <input type="Year" name="Year" placeholder="Year" onfocus="this.placeholder=''" onblur="this.placeholder='Year'" required/>
             <br><br>
             <input class="inputButton" type="submit" name="submit" value="Create your profile"/>
         </form>
