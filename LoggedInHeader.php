1f9956b221bd4ec7856658f51d662eeebcf5c7b1
jennifergowie <0308182@rgu.ac.uk>
2017-04-18 20:32:56 +0100

    Add files via upload

--- LoggedInHeader.php
+++ LoggedInHeader.php
@@ -7,7 +7,7 @@
 <body>
     <div style="position: absolute; top: 10px; right: 10px; height: 145px;">
         <span>Current User </span><?php echo "<b>{$_SESSION["username"]}</b>"?><br>
-        <span>Student Name </span><?php echo "<b>{$_SESSION["StudentName"]}</b>"?><br>
+        <span>Student Name </span><?php echo "<b>{$_SESSION["emailaddress"]}</b>"?><br>
     </div>
 
 </header>
