4fc979d8aaa3fcbcc7091a749ace33a2c017b431
jennifergowie <0308182@rgu.ac.uk>
2017-04-19 09:23:02 +0100

    Add files via upload

--- LoginValidation.php
+++ LoginValidation.php
@@ -16,34 +16,34 @@ if(empty($_POST) or empty($_POST["username"]) or empty($_POST["password"]))
     $userPasswordArray = $link->query($sqlPassword);
     $userPassword = getSingleValueFromDatabaseArray($userPasswordArray);
 
-    //$studentNameQuery = "SELECT StudentName FROM userprofiles WHERE UserName = '".$username."'";
-    //$studentNameinfo = $link->query($studentNameQuery);
-    //$studentName =getSingleValueFromDatabaseArray($studentNameinfo);
+    $studentNameQuery = "SELECT StudentName FROM userprofiles WHERE UserName = '".$username."'";
+    $studentNameinfo = $link->query($studentNameQuery);
+    $studentName =getSingleValueFromDatabaseArray($studentNameinfo);
 
-    //$emailAddressQuery = "SELECT EmailAddress FROM userprofiles WHERE UserName = '".$username."'";
-    //$emailAddressInfo = $link->query($emailAddressQuery);
-    //$emailAddress = getSingleValueFromDatabaseArray($emailAddressInfo);
+    $emailAddressQuery = "SELECT EmailAddress FROM userprofiles WHERE UserName = '".$username."'";
+    $emailAddressInfo = $link->query($emailAddressQuery);
+    $emailAddress = getSingleValueFromDatabaseArray($emailAddressInfo);
 
-    //$sqlMobileNumberQuery = "SELECT MobileNumber FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
-    //$mobileNumberInfo = $link->query($sqlMobileNumberQuery);
-    //$mobileNumber =getSingleValueFromDatabaseArray($mobileNumberInfo);
+    $sqlMobileNumberQuery = "SELECT MobileNumber FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
+    $mobileNumberInfo = $link->query($sqlMobileNumberQuery);
+    $mobileNumber =getSingleValueFromDatabaseArray($mobileNumberInfo);
 
-    //$sqlCourseQuery = "SELECT Course FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
-    //$courseInfo = $link->query($sqlCourseQuery);
-    //$course =getSingleValueFromDatabaseArray($courseInfo);
+    $sqlCourseQuery = "SELECT Course FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
+    $courseInfo = $link->query($sqlCourseQuery);
+    $course =getSingleValueFromDatabaseArray($courseInfo);
 
-    //$sqlYearQuery = "SELECT Year FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
-    //$yearInfo = $link->query($sqlYearQuery);
-    //$year =getSingleValueFromDatabaseArray($yearInfo);
+    $sqlYearQuery = "SELECT Year FROM userprofiles WHERE UserName = '".$username."'"; //Setup SQL query to get CustomerID from username
+    $yearInfo = $link->query($sqlYearQuery);
+    $year =getSingleValueFromDatabaseArray($yearInfo);
 
     if(checkPassword($password,$userPassword)){
         $_SESSION["username"]=$username;
         $_SESSION["password"]=$password;
-       // $_SESSION["studentName"]=$studentName;
-        //$_SESSION["email"]=$emailAddress;
-       // $_SESSION["mobileNumber"]=$mobileNumber;
-        //$_SESSION["course"]=$course;
-        //$_SESSION["year"]=$year;
+        $_SESSION["studentName"]=$studentName;
+        $_SESSION["email"]=$emailAddress;
+        $_SESSION["mobileNumber"]=$mobileNumber;
+        $_SESSION["course"]=$course;
+        $_SESSION["year"]=$year;
         header("location: /Home.php");
         exit();
     }
